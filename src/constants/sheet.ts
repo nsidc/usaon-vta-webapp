5dbcc80afcb8d6ccea4d01f515a05e0a9a5f75fd
Matt Fisher <matt.fisher@nsidc.org>
2022-10-05 10:30:23 -0600

    Use a test data sheet temporarily

--- src/constants/sheet.ts
+++ src/constants/sheet.ts
@@ -13,9 +13,17 @@ export const SHEET_URL_TEMPLATE = 'https://docs.google.com/spreadsheets/d/e/${sh
 // "published", but the tab identifiers aren't discoverable without access the
 // the original (not "pub") sheet. Need to request access or sheet IDs (see
 // `gid=?` in the URL). Only the first tab has a predictable ID (0)
-export const SHEET: Sheet  = {
-  // The URL of a _published_ sheet:
-  id: '2PACX-1vR3HNKcyZWsXCn5Idj1JNWKCzRz1e-IMXCuP6yN2ySi3D5-eu3dtDTOuDSEfxidl9LOvAj1zHyeksOU',
+// export const SHEET: Sheet = {
+//   // The URL of a _published_ sheet:
+//   id: '2PACX-1vR3HNKcyZWsXCn5Idj1JNWKCzRz1e-IMXCuP6yN2ySi3D5-eu3dtDTOuDSEfxidl9LOvAj1zHyeksOU',
+//   tab_id_nodes: '0',
+//   tab_id_edges: '1551812291',
+// }
+
+
+// Temporary test sheet
+export const SHEET: Sheet = {
+  id: '2PACX-1vRibsvVFJykLLBD6vjkO0hP8Y-Hbi7V4Mg4-YxX4TT2WyYp4x9aKJ7iKMnkyKnbKTh3BXKH_BnakmGC',
   tab_id_nodes: '0',
-  tab_id_edges: '1551812291',
+  tab_id_edges: '1577760007',
 }
